2084d27e06270a935e39a9e2c6e12abe405e3ee2
VitaCheung <ctw1207@gmail.com>
2023-03-28 12:31:43 -0700

    update my branch

---
+++ HospitalProject2/Models/ViewModels/UpdateVolunteers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HospitalProject2.Models.ViewModels
+{
+    public class UpdateVolunteers
+    {
+        public VolunteersDto SelectedVolunteer { get; set; }
+
+        public IEnumerable<DepartmentsDto> DepartmentOptions { get; set; }
+    }
+}
